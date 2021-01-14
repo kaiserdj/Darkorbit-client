c886e0af53f0e41aec76d39eaff71ae15f26c50e
Kaiserdj <gonferfr@gmail.com>
2021-01-14 14:27:27 +0100

    Client original useragent

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const userAgent = require("./useragent")
 
-let mainWindow;
+async function createWindow() {
+    let Useragent = await userAgent.getVersion()
 
-app.on('window-all-closed', function() {
-    if (process.platform != 'darwin')
-        app.quit();
-});
+    let mainWindow;
 
-let ppapi_flash_path;
+    let ppapi_flash_path;
 
-if (process.platform == 'win32') {
-    ppapi_flash_path = path.join(__dirname, 'pepflashplayer.dll')
-} else if (process.platform == 'linux') {
-    ppapi_flash_path = path.join(__dirname, 'libpepflashplayer.so')
-} else if (process.platform == 'darwin') {
-    ppapi_flash_path = path.join(__dirname, 'PepperFlashPlayer.plugin')
-}
+    if (process.platform == 'win32') {
+        ppapi_flash_path = path.join(__dirname, 'pepflashplayer.dll')
+    } else if (process.platform == 'linux') {
+        ppapi_flash_path = path.join(__dirname, 'libpepflashplayer.so')
+    } else if (process.platform == 'darwin') {
+        ppapi_flash_path = path.join(__dirname, 'PepperFlashPlayer.plugin')
+    }
 
-app.commandLine.appendSwitch('ppapi-flash-path', ppapi_flash_path)
+    app.commandLine.appendSwitch('ppapi-flash-path', ppapi_flash_path)
 
-app.on('ready', function() {
     mainWindow = new BrowserWindow({
         'width': 800,
         'height': 600,
@@ -29,7 +27,8 @@ app.on('ready', function() {
             'plugins': true
         },
     })
-    mainWindow.loadURL(`https://www.darkorbit.com/`, { userAgent: 'BigPointClient/1.4.6' })
+
+    mainWindow.loadURL(`https://www.darkorbit.com/`, { userAgent: Useragent })
 
     mainWindow.webContents.on('new-window', async function(e, url) {
         e.preventDefault()
@@ -40,6 +39,20 @@ app.on('ready', function() {
             }
         })
 
-        external.loadURL(url, { userAgent: 'BigPointClient/1.4.6' })
+        external.loadURL(url, { userAgent: Useragent })
     });
-});
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function() {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
---
+++ useragent.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const parseString = require('xml2js').parseString;
+
+const url = "http://darkorbit-22-client.bpsecure.com/bpflashclient/windows.x64/repository/Updates.xml"
+
+const httpGet = url => {
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            res.setEncoding('utf8');
+            let body = '';
+            res.on('data', chunk => body += chunk);
+            res.on('end', () => resolve(body));
+        }).on('error', reject);
+    });
+};
+
+function parseXml(xml) {
+    return new Promise((resolve, reject) => {
+        parseString(xml, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+async function getVersion() {
+    let xml = await httpGet(url);
+    let json = await parseXml(xml)
+
+    return `BigPointClient/${json.Updates.PackageUpdate[0].Version[0]}`
+}
+
+module.exports = {
+    getVersion
+}
