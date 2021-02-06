dd6625ff2f3890404285ee3beecbcaa204a96828
Kaiserdj <gonferfr@gmail.com>
2021-02-06 18:46:47 +0100

    Add search filter users

--- credentials/html/master.js
+++ credentials/html/master.js
@@ -31,6 +31,27 @@ function run() {
                 ipcRenderer.send('checkMaster', master);
             }
             break;
+        case "list":
+            document.getElementById("search").addEventListener('keyup', function() {
+                var input, filter, table, tr, td, i, txtValue;
+                input = document.getElementById("search");
+                filter = input.value.toUpperCase();
+                table = document.getElementById("table");
+                tr = table.getElementsByTagName("tr");
+
+                for (i = 0; i < tr.length; i++) {
+                    td = tr[i].getElementsByTagName("td")[1];
+                    if (td) {
+                        txtValue = td.textContent || td.innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                        } else {
+                            tr[i].style.display = "none";
+                        }
+                    }
+                }
+            });
+            break;
     }
 }
 
