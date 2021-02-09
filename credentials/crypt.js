0ff832d078c7c59f7d38e4bf0d8b97bd0142f6fc
Kaiserdj <gonferfr@gmail.com>
2021-02-09 21:42:04 +0100

    Custom Iv crypto

--- credentials/crypt.js
+++ credentials/crypt.js
@@ -2,17 +2,20 @@ const crypto = require('crypto');
 
 const algorithm = 'aes-256-ctr';
 const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
-const iv = crypto.randomBytes(16);
+let iv = crypto.randomBytes(16);
 
 const hash = (data) => {
     return crypto.createHash('md5').update(data).digest("hex");
 }
 
-const encrypt = (text) => {
+const encrypt = (data, customIv) => {
+    if (customIv) {
+        iv = Buffer.from(customIv, 'hex');
+    }
 
     const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
 
-    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
 
     return {
         iv: iv.toString('hex'),
@@ -20,11 +23,11 @@ const encrypt = (text) => {
     };
 };
 
-const decrypt = (hash) => {
+const decrypt = (data) => {
 
-    const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hash.iv, 'hex'));
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(data.iv, 'hex'));
 
-    const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
+    const decrpyted = Buffer.concat([decipher.update(Buffer.from(data.content, 'hex')), decipher.final()]);
 
     return decrpyted.toString();
 };
