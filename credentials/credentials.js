58b42418cb258026f7b4f0e0161333b9daa39216
Kaiserdj <gonferfr@gmail.com>
2021-02-10 17:53:38 +0100

    Add autologin

--- credentials/credentials.js
+++ credentials/credentials.js
@@ -13,7 +13,7 @@ class credentials {
 
         if (!settings.getSync().master) {
             this.createWindow("register");
-        }
+        };
 
         ipcMain.on('registerMaster', (event, pass) => {
             this.registerMaster(pass);
@@ -32,7 +32,65 @@ class credentials {
             if (!this.check) {
                 this.window.webContents.send("checkMasterRet", this.check);
             }
-        })
+        });
+
+        ipcMain.on('loadList', (event, arg) => {
+            let backup = this.settings.getSync();
+
+            if ("accounts" in backup) {
+                backup.accounts = crypt.decrypt({ "iv": this.hashMaster, "content": backup.accounts });
+            } else {
+                backup.accounts = [];
+            }
+
+            this.window.webContents.send("sendList", backup.accounts);
+        });
+
+        ipcMain.on('userRegister', (event, arg) => {
+            this.createWindow("userRegister");
+        });
+
+        ipcMain.on('registerUser', (event, data) => {
+            this.registerAccount(data);
+
+            this.createWindow("list");
+        });
+
+        ipcMain.on('editUser', (event, data) => {
+            let backup = this.settings.getSync();
+
+            backup.accounts = JSON.parse(crypt.decrypt({ "iv": this.hashMaster, "content": backup.accounts }));
+
+            let account = backup.accounts.find(o => o.id == data);
+
+            this.createWindow("editUser");
+
+            ipcMain.on('getEditUser', (event, e) => {
+                this.window.webContents.send("sendEditUser", [account.id, account.username]);
+            });
+        });
+
+        ipcMain.on('changeUser', (event, data) => {
+            this.editAccount(data);
+            this.createWindow("list");
+        });
+
+        ipcMain.on('deleteUser', (event, data) => {
+            this.deleteAccount(data);
+            this.createWindow("list");
+        });
+
+        ipcMain.on('loginUser', (event, data) => {
+            this.window.close();
+
+            let backup = this.settings.getSync();
+
+            backup.accounts = JSON.parse(crypt.decrypt({ "iv": this.hashMaster, "content": backup.accounts }));
+
+            let account = backup.accounts.find(o => o.id == data);
+
+            this.mainWindow.webContents.send("login", [account.username, account.password]);
+        });
     }
 
     createWindow(type) {
@@ -67,6 +125,16 @@ class credentials {
             case "list":
                 this.window.setSize(500, 500);
                 this.window.loadFile("./credentials/html/list.html");
+
+                break;
+            case "userRegister":
+                this.window.setSize(380, 400);
+                this.window.loadFile("./credentials/html/userRegister.html");
+
+                break;
+            case "editUser":
+                this.window.setSize(380, 480);
+                this.window.loadFile("./credentials/html/userEdit.html");
 
                 break;
         }
@@ -104,6 +172,70 @@ class credentials {
         }
 
         return this.check;
+    }
+
+    registerAccount(input) {
+        let backup = this.settings.getSync();
+
+        if ("accounts" in backup) {
+            backup.accounts = JSON.parse(crypt.decrypt({ "iv": this.hashMaster, "content": backup.accounts }));
+        } else {
+            backup.accounts = [];
+        }
+
+        let id = new Date().valueOf();
+
+        let account = {
+            id,
+            username: input[0],
+            password: input[1]
+        }
+
+        backup.accounts.push(account);
+
+        backup.accounts = crypt.encrypt(JSON.stringify(backup.accounts), this.hashMaster).content;
+
+        this.settings.setSync(backup);
+
+        this.createWindow("list");
+    }
+
+    editAccount(input) {
+        let backup = this.settings.getSync();
+
+        backup.accounts = JSON.parse(crypt.decrypt({ "iv": this.hashMaster, "content": backup.accounts }));
+
+        let accountIndex = backup.accounts.findIndex(x => x.id == input[0]);
+
+        backup.accounts[accountIndex] = {
+            id: input[0],
+            username: input[1],
+            password: input[2]
+        };
+
+        backup.accounts = crypt.encrypt(JSON.stringify(backup.accounts), this.hashMaster).content;
+
+        this.settings.setSync(backup);
+    }
+
+    deleteAccount(id) {
+        let backup = this.settings.getSync();
+
+        backup.accounts = JSON.parse(crypt.decrypt({ "iv": this.hashMaster, "content": backup.accounts }));
+
+        let newAccounts = [];
+
+        for(let elem of backup.accounts) {
+            if(elem.id !== id) {
+                newAccounts.push(elem);
+            }
+        }
+
+        backup.accounts = newAccounts;
+
+        backup.accounts = crypt.encrypt(JSON.stringify(backup.accounts), this.hashMaster).content;
+
+        this.settings.setSync(backup);
     }
 }
 
--- credentials/html/master.js
+++ credentials/html/master.js
@@ -32,7 +32,9 @@ function run() {
             }
             break;
         case "list":
-            document.getElementById("search").addEventListener('keyup', function() {
+            ipcRenderer.send('loadList', true);
+
+            document.getElementById("search").addEventListener('keyup', () => {
                 var input, filter, table, tr, td, i, txtValue;
                 input = document.getElementById("search");
                 filter = input.value.toUpperCase();
@@ -40,7 +42,7 @@ function run() {
                 tr = table.getElementsByTagName("tr");
 
                 for (i = 0; i < tr.length; i++) {
-                    td = tr[i].getElementsByTagName("td")[1];
+                    td = tr[i].getElementsByTagName("td")[2];
                     if (td) {
                         txtValue = td.textContent || td.innerText;
                         if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -51,12 +53,77 @@ function run() {
                     }
                 }
             });
+
+            document.getElementById("new").addEventListener("click", () => {
+                ipcRenderer.send('userRegister', null);
+            });
+            break;
+        case "registerUser":
+            document.getElementById("form").onsubmit = (sub) => {
+                sub.preventDefault();
+                let username = document.getElementById("username").value;
+                let password = document.getElementById("password").value;
+
+                ipcRenderer.send('registerUser', [username, password]);
+            }
+            break;
+        case "editUser":
+            ipcRenderer.send('getEditUser', true);
+
+            document.getElementById("form").onsubmit = (sub) => {
+                sub.preventDefault();
+                let username = document.getElementById("username").value;
+                let password = document.getElementById("password").value;
+
+                if (username !== "" && password !== "") {
+                    ipcRenderer.send('changeUser', [global.id, username, password]);
+                }
+            };
+
+            document.getElementById("delete").addEventListener("click", () => {
+                if (!confirm("Are you sure to delete the account data?")) {
+                    return;
+                }
+
+                ipcRenderer.send('deleteUser', global.id);
+            });
             break;
     }
 }
 
-ipcRenderer.on("checkMasterRet", function(event, ret) {
-    if (!ret) {
+ipcRenderer.on("checkMasterRet", function(event, data) {
+    if (!data) {
         alert("Incorrect password");
     }
 });
+
+ipcRenderer.on("sendList", function(event, data) {
+    let accounts = JSON.parse(data);
+    let i = 0;
+    for (let account of accounts) {
+        i++;
+        let table = document.getElementById('table').getElementsByTagName('tbody')[0];
+        let newRow = table.insertRow();
+        newRow.innerHTML = `
+            <td class="id">${account.id}</td>
+            <td>${i}</td>
+            <td>${account.username}</td>
+            <td>&nbsp;<i class="bi bi-pencil-square edit"></i></td>
+            <td>&nbsp;<i class="bi bi-key login"></i></td>    
+        `;
+    }
+    const edit = document.querySelectorAll('.edit');
+    edit.forEach(el => el.addEventListener('click', event => {
+        ipcRenderer.send('editUser', event.target.parentNode.parentNode.querySelectorAll(".id")[0].innerText);
+    }));
+
+    const login = document.querySelectorAll('.login');
+    login.forEach(el => el.addEventListener('click', event => {
+        ipcRenderer.send('loginUser', event.target.parentNode.parentNode.querySelectorAll(".id")[0].innerText);
+    }));
+});
+
+ipcRenderer.on("sendEditUser", function(event, data) {
+    global.id = data[0];
+    document.getElementById("username").value = data[1];
+})
