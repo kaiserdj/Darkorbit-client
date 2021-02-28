20f2e06eb544a06e4e43469516afe8c0c3768d7f
Kaiserdj <gonferfr@gmail.com>
2021-02-28 15:02:54 +0100

    Fix ppapi detection on linux

--- core.js
+++ core.js
@@ -22,7 +22,7 @@ class Core {
         if (process.platform == 'win32') {
             this.ppapi_flash_path = path.join(app.getAppPath(), '../flash/pepflashplayer.dll');
         } else if (process.platform == 'linux') {
-            this.ppapi_flash_path = path.join(process.resourcesPath, './flash/libpepflashplayer.so');
+            this.ppapi_flash_path = path.join(process.resourcesPath.split("/")[1] === "tmp" ? process.resourcesPath : app.getAppPath(), './flash/libpepflashplayer.so');
             this.app.commandLine.appendSwitch("--no-sandbox");
         } else if (process.platform == 'darwin') {
             this.ppapi_flash_path = path.join(app.getAppPath(), '../flash/PepperFlashPlayer.plugin');
