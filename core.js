667b0938ebde6b5eb785465fd7f048009606a1b3
Kaiserdj <gonferfr@gmail.com>
2021-02-28 16:36:32 +0100

    Fix ppapi detection on windows and mac

--- core.js
+++ core.js
@@ -1,5 +1,6 @@
 const { app } = require('electron');
 const path = require("path");
+const fs = require('fs');
 
 class Core {
     constructor() {
@@ -21,11 +22,17 @@ class Core {
     ppapi() {
         if (process.platform == 'win32') {
             this.ppapi_flash_path = path.join(app.getAppPath(), '../flash/pepflashplayer.dll');
+            if (!fs.existsSync(this.ppapi_flash_path)) {
+                this.ppapi_flash_path = path.join(app.getAppPath(), './flash/pepflashplayer.dll');
+            }
         } else if (process.platform == 'linux') {
             this.ppapi_flash_path = path.join(process.resourcesPath.split("/")[1] === "tmp" ? process.resourcesPath : app.getAppPath(), './flash/libpepflashplayer.so');
             this.app.commandLine.appendSwitch("--no-sandbox");
         } else if (process.platform == 'darwin') {
-            this.ppapi_flash_path = path.join(app.getAppPath(), '../flash/PepperFlashPlayer.plugin');
+            this.ppapi_flash_path = path.join(app.getAppPath(), `../flash/PepperFlashPlayer.plugin`);
+            if (!fs.existsSync(this.ppapi_flash_path)) {
+                this.ppapi_flash_path = path.join(app.getAppPath(), './flash/PepperFlashPlayer.plugin');
+            }
         }
 
         this.app.commandLine.appendSwitch('ppapi-flash-path', this.ppapi_flash_path);
